af3fa3376b1b491c8002432e1b84fb1987e275c2
Sean Smith <seaninboulder@gmail.com>
2011-12-08 23:43:22 -0700

    Cleaning up main_index view

--- application/views/main_index.php
+++ application/views/main_index.php
@@ -1,15 +1,10 @@
-<?php 
-// Require header.php at the top of every page
-require_once('header.php'); 
-?>
-
 <div id="accordion">
 	<?php
-		foreach($category->result() as $c) { ?>
+		foreach($category->result() as $c): ?>
 			<h3>
-				<a><? echo $c->cid . ' ' . $c->title; ?></a>
+				<a><?= $c->cid . ' ' . $c->title; ?></a>
 				<br />
-				<? echo $c->updated; ?>
+				<?= $c->updated; ?>
 			</h3>
 			<div>
 				<p>
@@ -17,18 +12,13 @@ require_once('header.php');
 					<ul>
 						<? $i=0; foreach($topic[$c->cid]->result() as $t) { ?>
 							<li>
-								<a href="/main/topic/<? echo $t->tid; ?>/"><? echo $t->title; $i++; ?></a>
+								<a href="/main/topic/<?= $t->tid ?>/"><?= $t->title; $i++; ?></a>
 								<br />
-								<? echo $t->updated; ?>
+								<?= $t->updated; ?>
 							</li>
 						<? } if ($i==0) echo '<li>No Topics</li>'; ?> 
 					</ul>
 				</p>
 			</div>
-	<?php } ?>
+	<?php endforeach; ?>
 </div>
-
-<?php 
-// Require footer.php at the bottom of every page
-require_once('footer.php'); 
-?>
